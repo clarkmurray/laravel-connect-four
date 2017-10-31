72aab6e699e4a76d566b4ce4c6ea62613325ee93
Clark Murray <clarkamurray@gmail.com>
2017-10-31 15:39:20 -0400

    Working on board

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+	$players = ['Red', 'Blue'];
+	$turn = 5;
+	$currentPlayer = 'Red';
+	$board = [];
+	$rows = 6;
+	$columns = 7;
+	for ($r=0; $r < $rows; $r++) {
+		for ($c = 0; $columns < 7; $c++) {
+			$board[$r][$c]['color'] = 'red';
+		}
+	}
+
+    return view('board', compact('currentPlayer', 'board', 'rows', 'columns')
+		);
+});
